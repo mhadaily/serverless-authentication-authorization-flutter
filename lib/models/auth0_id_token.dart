a86ac009a6a5ea35e6b28b26a1be58fed299da9b
Majid <mhadaily@gmail.com>
2021-07-25 15:41:51 +0200

    add auth0_id_token

---
+++ lib/models/auth0_id_token.dart
@@ -0,0 +1,46 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'auth0_id_token.g.dart';
+
+@JsonSerializable()
+class Auth0IdToken {
+  Auth0IdToken({
+    required this.nickname,
+    required this.name,
+    required this.email,
+    required this.picture,
+    required this.updatedAt,
+    required this.iss,
+    required this.sub,
+    required this.aud,
+    required this.iat,
+    required this.exp,
+    required this.authTime,
+  });
+
+  final String nickname;
+  final String name;
+  final String picture;
+
+  @JsonKey(name: 'updated_at')
+  final String updatedAt;
+
+  final String iss;
+
+  // userID getter to understand it easier
+  String get userId => sub;
+  final String sub;
+
+  final String aud;
+  final String email;
+  final int iat;
+  final int exp;
+
+  @JsonKey(name: 'auth_time')
+  final int authTime;
+
+  factory Auth0IdToken.fromJson(Map<String, dynamic> json) =>
+      _$Auth0IdTokenFromJson(json);
+
+  Map<String, dynamic> toJson() => _$Auth0IdTokenToJson(this);
+}
---
+++ lib/models/auth0_id_token.g.dart
@@ -0,0 +1,38 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'auth0_id_token.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Auth0IdToken _$Auth0IdTokenFromJson(Map<String, dynamic> json) {
+  return Auth0IdToken(
+    nickname: json['nickname'] as String,
+    name: json['name'] as String,
+    email: json['email'] as String,
+    picture: json['picture'] as String,
+    updatedAt: json['updated_at'] as String,
+    iss: json['iss'] as String,
+    sub: json['sub'] as String,
+    aud: json['aud'] as String,
+    iat: json['iat'] as int,
+    exp: json['exp'] as int,
+    authTime: json['auth_time'] as int,
+  );
+}
+
+Map<String, dynamic> _$Auth0IdTokenToJson(Auth0IdToken instance) =>
+    <String, dynamic>{
+      'nickname': instance.nickname,
+      'name': instance.name,
+      'picture': instance.picture,
+      'updated_at': instance.updatedAt,
+      'iss': instance.iss,
+      'sub': instance.sub,
+      'aud': instance.aud,
+      'email': instance.email,
+      'iat': instance.iat,
+      'exp': instance.exp,
+      'auth_time': instance.authTime,
+    };
