72495e2c841753d8518446c29a466c94f32fa323
Majid <mhadaily@gmail.com>
2021-07-25 14:16:11 +0200

    add refresh token and refine auth0 service

--- lib/helpers/constants.dart
+++ lib/helpers/constants.dart
@@ -47,3 +47,4 @@ const AUTH0_CLIENT_ID = String.fromEnvironment('AUTH0_CLIENT_ID');
 const AUTH0_ISSUER = 'https://$AUTH0_DOMAIN';
 const BUNDLE_IDENTIFIER = 'mj.coffee.app';
 const AUTH0_REDIRECT_URI = '$BUNDLE_IDENTIFIER://login-callback';
+const REFRESH_TOKEN_KEY = 'refresh_token';
--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_appauth/flutter_appauth.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:mjcoffee/helpers/constants.dart';
 
 class AuthService {
   static final AuthService instance = AuthService._internal();
@@ -8,4 +9,22 @@ class AuthService {
 
   final FlutterAppAuth appAuth = FlutterAppAuth();
   final FlutterSecureStorage secureStorage = const FlutterSecureStorage();
+
+  login() async {
+    // construct Authorization Token Request
+    final authorizationTokenRequest = AuthorizationTokenRequest(
+      AUTH0_CLIENT_ID,
+      AUTH0_REDIRECT_URI,
+      issuer: 'https://$AUTH0_DOMAIN',
+      scopes: ['openid', 'profile', 'offline_access', 'email'],
+    );
+
+    // Call code exchange endpoint to get an authorization token
+    final AuthorizationTokenResponse? result =
+        await appAuth.authorizeAndExchangeCode(
+      authorizationTokenRequest,
+    );
+
+    print(result);
+  }
 }
