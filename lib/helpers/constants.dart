853384f2c15227672e8d0089b81887d654c32770
Majid <mhadaily@gmail.com>
2021-07-25 22:08:00 +0200

    add stteam chat key

--- lib/helpers/constants.dart
+++ lib/helpers/constants.dart
@@ -48,3 +48,4 @@ const AUTH0_ISSUER = 'https://$AUTH0_DOMAIN';
 const BUNDLE_IDENTIFIER = 'mj.coffee.app';
 const AUTH0_REDIRECT_URI = '$BUNDLE_IDENTIFIER://login-callback';
 const REFRESH_TOKEN_KEY = 'refresh_token';
+const STREAM_API_KEY = String.fromEnvironment('STREAM_API_KEY');
